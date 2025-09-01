184478bbb308c494a565ba923b9393e9cbc6519a
BeCare666 <becare.fr.ge@gmail.com>
2025-09-01 09:56:11 +0100

    cv

--- src/components/authors/about.tsx
+++ src/components/authors/about.tsx
@@ -10,15 +10,18 @@ import { getIcon } from '@/lib/get-icon';
 
 const About = ({ shop }: { shop: Shop }) => {
   const {
-    description,
-    name,
+    description = '',
+    name = '',
     address,
     owner,
-    orders_count,
-    products_count,
-    settings: { socials },
+    orders_count = 0,
+    products_count = 0,
+    settings,
   } = shop;
+
+  const socials = settings?.socials || [];
   const { t } = useTranslation('common');
+
   return (
     <motion.div
       variants={fadeInBottom()}
@@ -30,22 +33,27 @@ const About = ({ shop }: { shop: Shop }) => {
         </h2>
         <p className="leading-6">{description}</p>
         <div className="space-y-3.5 pt-4 text-dark/80 dark:text-light/80 md:pt-6 xl:pt-7">
-          <address className="flex max-w-sm items-start not-italic leading-[1.8]">
-            <span className="mt-[3px] w-7 shrink-0 text-dark-800 dark:text-light-900">
-              <MapPinIcon className="h-4 w-4" />
-            </span>
-            {formatAddress(address)}
-          </address>
-          <div className="flex items-center">
-            <span className="w-7 shrink-0 text-dark-800 dark:text-light-900">
-              <AtIcon className="h-4 w-4" />
-            </span>
-            <a href={`mailto:${owner?.email}`} className="hover:text-brand">
-              {owner?.email}
-            </a>
-          </div>
+          {address && (
+            <address className="flex max-w-sm items-start not-italic leading-[1.8]">
+              <span className="mt-[3px] w-7 shrink-0 text-dark-800 dark:text-light-900">
+                <MapPinIcon className="h-4 w-4" />
+              </span>
+              {formatAddress(address)}
+            </address>
+          )}
+          {owner?.email && (
+            <div className="flex items-center">
+              <span className="w-7 shrink-0 text-dark-800 dark:text-light-900">
+                <AtIcon className="h-4 w-4" />
+              </span>
+              <a href={`mailto:${owner.email}`} className="hover:text-brand">
+                {owner.email}
+              </a>
+            </div>
+          )}
         </div>
       </div>
+
       <div className="mt-7 flex-shrink-0 rounded-md bg-light p-6 shadow-card dark:bg-dark-250 md:mt-0 md:w-72 lg:p-8">
         <div className="-mx-2 flex pb-6 lg:pb-7">
           <div className="flex flex-shrink-0 flex-col px-2 pr-10 text-13px capitalize text-dark-500 dark:text-light-900 lg:w-1/2 lg:pr-0">
@@ -61,27 +69,30 @@ const About = ({ shop }: { shop: Shop }) => {
             {t('text-products')}
           </div>
         </div>
-        <div className="space-y-3 border-t border-light-300 pt-5 dark:border-dark-500">
-          {socials.map(({ icon, url }, idx) => (
-            <a
-              key={idx}
-              href={url}
-              target="_blank"
-              rel="noreferrer"
-              className="group flex items-center"
-            >
-              {getIcon({
-                iconList: socialIcons,
-                iconName: icon,
-                className:
-                  'w-3.5 h-3.5 text-dark-800 dark:text-light-900 shrink-0',
-              })}
-              <span className="transition-colors group-hover:text-dark ltr:pl-2 rtl:pr-2 dark:group-hover:text-light">
-                {url.slice(12, -1).split('/').slice(0, 1)}
-              </span>
-            </a>
-          ))}
-        </div>
+
+        {socials.length > 0 && (
+          <div className="space-y-3 border-t border-light-300 pt-5 dark:border-dark-500">
+            {socials.map(({ icon, url }, idx) => (
+              <a
+                key={idx}
+                href={url}
+                target="_blank"
+                rel="noreferrer"
+                className="group flex items-center"
+              >
+                {getIcon({
+                  iconList: socialIcons,
+                  iconName: icon,
+                  className:
+                    'w-3.5 h-3.5 text-dark-800 dark:text-light-900 shrink-0',
+                })}
+                <span className="transition-colors group-hover:text-dark ltr:pl-2 rtl:pr-2 dark:group-hover:text-light">
+                  {url.replace(/^https?:\/\//, '').split('/')[0]}
+                </span>
+              </a>
+            ))}
+          </div>
+        )}
       </div>
     </motion.div>
   );
