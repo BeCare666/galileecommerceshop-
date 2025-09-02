baae372480c52b4d86eaa6ec0804be1a2d0f9953
BeCare666 <becare.fr.ge@gmail.com>
2025-09-02 12:37:35 +0100

    gh

--- src/components/mapChart/mapChart.tsx
+++ src/components/mapChart/mapChart.tsx
@@ -42,7 +42,10 @@ const MapWithCorridors: React.FC<MapChartProps> = (
           },
         });
 
-        if (res) {
+        if (res.data) {
+          if (corridorsTable.length > 0) {
+            setMapIsOk(false);
+          }
           const corridors = res.data;
           corridorsTable.push(...corridors);
           const countryCodes = new Set<string>();
@@ -50,7 +53,6 @@ const MapWithCorridors: React.FC<MapChartProps> = (
           const fromCountryIdsNX = fromCountryIdsN[0].map((c: any) => c);
 
           fromCountryIdsNX.forEach((corridor: any) => {
-            setMapIsOk(false);
             if (corridor.from_countries_code)
               countryCodes.add(corridor.from_countries_code);
             if (corridor.to_countries_code)
