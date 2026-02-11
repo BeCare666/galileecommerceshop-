594a80f015dd6f350ae9bd3698df2489f30359f1
BeCare666 <becare.fr.ge@gmail.com>
2026-02-11 17:05:20 +0100

    products/tags update

--- src/pages/products/tags/index.tsx
+++ src/pages/products/tags/index.tsx
@@ -4,10 +4,10 @@ export default function TagsIndex() {
     return null;
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
     return {
         redirect: {
-            destination: "/",
+            destination: locale ? `/${locale}` : "/",
             permanent: false,
         },
     };
